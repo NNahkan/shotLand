a474ff92a1e3a1b2a19b9941d7d8d9b74606a5a7
NNahkan <hoksuz2424@gmail.com>
2023-07-24 22:09:20 -0400

    first commit

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+	return (
+		<div>
+			<div>
+				<input type="text" />
+				<br />
+				<button>Coctail</button>
+				<button>Shot</button>
+				<button>Something</button>
+			</div>
+			<div>coctails</div>
+		</div>
+	)
+}
+
+export default Home
